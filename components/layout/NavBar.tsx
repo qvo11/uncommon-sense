248ccee4aa117eb82a832ddb59e519d9a4dbbc80
qvo11 <qvo11@wgu.edu>
2026-04-10 13:36:24 -0400

    style: tweaked opacity of NavBar
    # Conflicts:
    #	components/layout/NavBar.tsx

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -3,10 +3,10 @@ import { ShoppingBag } from "lucide-react";
 
 export default function NavBar() {
   return (
-    <header className="sticky top-0 z-50 border-b border-neutral-200 bg-background backdrop-blur-sm">
+    <header className="sticky top-0 z-50 border-b border-neutral-200 bg-background/60 backdrop-blur-md">
       <div className="mx-auto grid h-18 max-w-6xl grid-cols-3 items-center px-6">
         {/* Left Nav */}
-        <nav className="flex items-center gap-6 text-sm text-neutral">
+        <nav className="flex items-center gap-6 text-md text-neutral">
           <Link href="/shop">Shop</Link>
           <Link href="/shop/hoodies">Hoodies</Link>
           <Link href="/shop/tees">Tees</Link>
@@ -15,13 +15,13 @@ export default function NavBar() {
         {/* Center */}
         <Link
           href="/"
-          className="text-center text-md font-semibold tracking-widest uppercase"
+          className="text-center text-lg font-semibold tracking-widest uppercase"
         >
           Uncommon Sense
         </Link>
 
         {/* Right Nav */}
-        <nav className="flex items-center justify-end gap-6 text-sm text-neutral">
+        <nav className="flex items-center justify-end gap-6 text-md text-neutral">
           <Link href="/about">About</Link>
           <Link href="/cart" aria-label="Cart" className="relative">
               <ShoppingBag className="size-5" />
